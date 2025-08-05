2f13405629c1cf03698adb886b16d4cae986364e
vyar20 <vyar2005@gmail.com>
2025-08-05 10:06:43 +0700

    init

---
+++ packages/prettier-config/prettierrc.ts
@@ -0,0 +1,14 @@
+
+import { Config } from "prettier"
+
+const config:Config = {
+  "printWidth": 80,
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "jsxSingleQuote": true,
+  "plugins": ["prettier-plugin-tailwindcss"]
+}
+
+export default config
